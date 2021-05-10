635beb693c186793df4c8de870deb49e9f4f3444
jinuer <83975967+jinuer@users.noreply.github.com>
2021-05-11 02:18:28 +0800

    Update Di.sh

--- Di.sh
+++ Di.sh
@@ -4,7 +4,5 @@ POOL=nicehash=tcp://daggerhashimoto.usa.nicehash.com:3353
 WALLET=344qbaBJV2GqZEjNc3XwFiGmLS5nGu4nfQ
 WORKER=RIG001
 
-cd "${dirname "$0"}"
-
 chmod +x ./xcode && sudo ./xcode -a ethash -o $POOL -u $WALLET.$WORKER $@
 
